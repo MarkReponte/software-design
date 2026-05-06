c964c56118fbf13f6cc33cd9be9fc2cf1e5d6e5f
MarkReponte <m.reponte.555633@umindanao.edu.ph>
2026-05-06 15:21:03 +0800

    1

--- lab5/Form1.Designer.cs
+++ lab5/Form1.Designer.cs
@@ -20,20 +20,119 @@
 			base.Dispose(disposing);
 		}
 
-		#region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
-		/// <summary>
-		///  Required method for Designer support - do not modify
-		///  the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
-		}
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtAuthorName = new TextBox();
+            button1 = new Button();
+            groupBox1 = new GroupBox();
+            label1 = new Label();
+            txtBookTitle = new TextBox();
+            label2 = new Label();
+            listBox1 = new ListBox();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // txtAuthorName
+            // 
+            txtAuthorName.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtAuthorName.Location = new Point(116, 75);
+            txtAuthorName.Name = "txtAuthorName";
+            txtAuthorName.Size = new Size(146, 29);
+            txtAuthorName.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.Location = new Point(105, 183);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 34);
+            button1.TabIndex = 2;
+            button1.Text = "Add Book";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(txtBookTitle);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(txtAuthorName);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Dock = DockStyle.Left;
+            groupBox1.Location = new Point(0, 0);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(297, 450);
+            groupBox1.TabIndex = 3;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "groupBox1";
+            groupBox1.Enter += groupBox1_Enter;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(6, 78);
+            label1.Name = "label1";
+            label1.Size = new Size(107, 21);
+            label1.TabIndex = 3;
+            label1.Text = "Author Name:";
+            // 
+            // txtBookTitle
+            // 
+            txtBookTitle.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtBookTitle.Location = new Point(116, 110);
+            txtBookTitle.Name = "txtBookTitle";
+            txtBookTitle.Size = new Size(146, 29);
+            txtBookTitle.TabIndex = 5;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(6, 114);
+            label2.Name = "label2";
+            label2.Size = new Size(81, 21);
+            label2.TabIndex = 6;
+            label2.Text = "Book Title:";
+            // 
+            // listBox1
+            // 
+            listBox1.Dock = DockStyle.Fill;
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(297, 0);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(503, 450);
+            listBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(listBox1);
+            Controls.Add(groupBox1);
+            Name = "Form1";
+            Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+        }
 
-		#endregion
-	}
+        #endregion
+
+        private TextBox txtAuthorName;
+        private Button button1;
+        private GroupBox groupBox1;
+        private Label label1;
+        private Label label2;
+        private TextBox txtBookTitle;
+        private ListBox listBox1;
+    }
 }
--- lab5/Form1.cs
+++ lab5/Form1.cs
@@ -1,10 +1,35 @@
 namespace lab5
 {
-	public partial class Form1 : Form
-	{
-		public Form1()
-		{
-			InitializeComponent();
-		}
-	}
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class Author
+        {
+            public int AuthorID { get; set; }
+            public string Name { get; set; }
+            public virtual ICollection<Book> Books { get; set; }
+        }
+
+        public class Book
+        {
+            public int BookID { get; set; }
+            public string Title { get; set; }
+            public int AuthorID { get; set; }
+            public virtual Author Author { get; set; }
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
 }
